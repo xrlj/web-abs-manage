43a92e12b7bf30162f86beec98ad217fe3e95277
xinxiamu <932852117@qq.com>
2019-11-12 21:58:40 +0800

    初始化项目

--- src/app/theme/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './app-header/app-header.component';
-export * from './app-body/app-body.component';
-export * from './app-aside/app-aside.component';
-export * from './app-footer/app-footer.component';
