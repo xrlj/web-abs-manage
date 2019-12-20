2de4b5445b6395f9af17aec1cc963c4f71a38bd2
xinxiamu <932852117@qq.com>
2019-12-20 11:19:10 +0800

    好好学习，天天向上。

--- src/app/pages/data-entry/tree-select/tree-select.component.ts
+++ src/app/pages/data-entry/tree-select/tree-select.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NzTreeNodeOptions, NzFormatEmitEvent, NzTreeComponent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-tree-select',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreeSelectComponent implements OnInit {
 
+  @ViewChild('nzTreeComponent', { static: false }) nzTreeComponent: NzTreeComponent;
+  defaultCheckedKeys = ['10020'];
+  defaultSelectedKeys = ['10010'];
+  defaultExpandedKeys = ['100', '1001'];
+
+  nodes: NzTreeNodeOptions[] = [
+    {
+      title: 'parent 1',
+      key: '100',
+      children: [
+        {
+          title: 'parent 1-0',
+          key: '1001',
+          disabled: true,
+          children: [
+            { title: 'leaf 1-0-0', key: '10010', disableCheckbox: true, isLeaf: true },
+            { title: 'leaf 1-0-1', key: '10011', isLeaf: true }
+          ]
+        },
+        {
+          title: 'parent 1-1',
+          key: '1002',
+          children: [
+            { title: 'leaf 1-1-0', key: '10020', isLeaf: true },
+            { title: 'leaf 1-1-1', key: '10021', isLeaf: true }
+          ]
+        }
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  nzClick(event: NzFormatEmitEvent): void {
+    console.log(event);
+  }
+
+  nzCheck(event: NzFormatEmitEvent): void {
+    console.log(event);
+  }
+
+  // nzSelectedKeys change
+  nzSelect(keys: string[]): void {
+    console.log(keys, this.nzTreeComponent.getSelectedNodeList());
   }
 
 }
